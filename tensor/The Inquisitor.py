f57a18029193ac971ce86e775371798bab5fe1c6
remysedlak <remysedlak@gmail.com>
2025-02-03 18:17:52 -0500

    jupyter notebook setup and inquisitor changes

--- tensor/The Inquisitor.py
+++ tensor/The Inquisitor.py
@@ -1,91 +1,103 @@
-#Import packages
+# Import packages
 import numpy as np
 import matplotlib.pyplot as plt
-import keras
-import tensorflow
+import os
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-#Height and Width refer to the size of the image
-#Channels refers to the amount of color channels (red, green, blue)
-
-#Image dimensions function
+# Image dimensions
 im_dim = {"height": 256, "width": 256, "channels": 3}
 
-#Create a classifier class
-
+# Create a classifier class
 class Classifier:
-    def __init__():
-        self.model = 0
-    
+    def __init__(self):
+        self.model = None
+
     def predict(self, x):
-        return self.model.pred(x)
-    
-    def fit(self, x, y):
-        return self.model.train_on_batch(x, y)
+        return self.model.predict(x)
+
+    def fit(self, x, y, epochs=10, batch_size=32):
+        return self.model.fit(x, y, epochs=epochs, batch_size=batch_size)
 
     def get_accuracy(self, x, y):
-        return self.model.test_on_batch(x, y)
-    
+        return self.model.evaluate(x, y)
+
     def load(self, path):
         self.model.load_weights(path)
 
-#Creata MesoNet class using Classifier
-
+# Create MesoNet class using Classifier
 class Meso(Classifier):
-    def __init__(slef, learning_rate = 0.001):
-        self.model = self.init.model()
-        optimizer = Adam(lr = learning_rate)
-        self.model.compile(optimizer = optimizer,
-                           loss = "mean_squared_error",
-                           netrics = ['accuracy'])
-    
+    def __init__(self, learning_rate=0.001):
+        super().__init__()
+        self.model = self.init_model()
+        optimizer = Adam(learning_rate=learning_rate)
+        self.model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=['accuracy'])
+
     def init_model(self):
-        x = Input(shape = (im_dim['height'],
-                           im_dim['width'],
-                           im_dim['channel']))
-        
-        x1 = Conv2D(8, (3,3), padding = 'same', activation = 'relu')(x)
+        x = Input(shape=(im_dim['height'], im_dim['width'], im_dim['channels']))
+
+        x1 = Conv2D(8, (3, 3), padding='same', activation='relu')(x)
         x1 = BatchNormalization()(x1)
-        x1 = MaxPooling2D(pool_size=(2,2), padding = 'same')(x1)
+        x1 = MaxPooling2D(pool_size=(2, 2), padding='same')(x1)
 
-        x2 = Conv2D(8, (5,5), padding = 'same', activation = 'relu')(x1)
+        x2 = Conv2D(8, (5, 5), padding='same', activation='relu')(x1)
         x2 = BatchNormalization()(x2)
-        x2 = MaxPooling2D(pool_size=(2,2), padding = 'same')(x2)
+        x2 = MaxPooling2D(pool_size=(2, 2), padding='same')(x2)
 
-        x3 = Conv2D(16, (5,5), padding = 'same', activation = 'relu')(x2)
+        x3 = Conv2D(16, (5, 5), padding='same', activation='relu')(x2)
         x3 = BatchNormalization()(x3)
-        x3 = MaxPooling2D(pool_size=(2,2), padding = 'same')(x3)
+        x3 = MaxPooling2D(pool_size=(2, 2), padding='same')(x3)
 
-        x4 = Conv2D(8, (3,3), padding = 'same', activation = 'relu')(x3)
+        x4 = Conv2D(8, (3, 3), padding='same', activation='relu')(x3)
         x4 = BatchNormalization()(x4)
-        x4 = MaxPooling2D(pool_size=(2,2), padding = 'same')(x4)
+        x4 = MaxPooling2D(pool_size=(2, 2), padding='same')(x4)
 
-        y = Flatten()(x4)
-        y = Dropout(0.5)(y)
-        y = Dense(16)(y)
-        y = LeakyReLU(alpha = 0.1)(y)
-        y = Dropout(0.5)(y)
-        y = Dense(1, activaton = "sigmoid")(y)
+        flat = Flatten()(x4)
+        dense1 = Dense(16, activation='relu')(flat)
+        drop = Dropout(0.5)(dense1)
+        output = Dense(1, activation='sigmoid')(drop)
 
-        return Model(inputs = x, outputs = y)
+        return Model(inputs=x, outputs=output)
 
-#Instantiate a MesoNet model with pretrained weights
-meso = Meso4()
-meso.load('./weights/Meso4_DF')
+# Data Loading Function
+def load_data(directory):
+    images, labels = [], []
+    for label, folder in enumerate(['real', 'fake']):
+        folder_path = os.path.join(directory, folder)
+        for filename in os.listdir(folder_path):
+            img_path = os.path.join(folder_path, filename)
+            img = load_img(img_path, target_size=(im_dim['height'], im_dim['width']))
+            img_array = img_to_array(img) / 255.0
+            images.append(img_array)
+            labels.append(label)  # 0 for real, 1 for fake
+    return np.array(images), np.array(labels)
 
-#Prepare the image we're classifing
+# Prediction Function
+def predict_image(model, image_path):
+    img = load_img(image_path, target_size=(im_dim['height'], im_dim['width']))
+    img_array = img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
-#Rescaling pixel values (between 1 and 255) to a range between 0 and 1
-Data_Generator = Image_Data_Generator(rescale = 1.0/255)
+    prediction = model.predict(img_array)[0][0]
+    label = "Real" if prediction < 0.5 else "Deepfake"
 
-#Instantiate the generator to feed the network the images we're trying to classify
-Generator = Data_Generator.flow_from_directory(
-    './data/',
-    target_size = (256,256),
-    batch_size = 1,
-    class_mode = 'binary')
+    plt.imshow(img)
+    plt.title(f'Prediction: {label}')
+    plt.axis('off')
+    plt.show()
 
-#Check the class assignments
-Generator.class_indices
+# Training the Model
+train_images, train_labels = load_data('train_data')
+model = Meso()
+print("Starting model training...")
+model.fit(train_images, train_labels, epochs=10, batch_size=32)
+print("Model training complete.")
 
-#Remove hidden files from Jupyter files
-!rmdir /s /q c:data\.ipynb_checkpoints
+# Testing the Model
+print("Testing on Real Image:")
+predict_image(model.model, 'test_data/real/sample.jpg')
+
+print("Testing on Fake Image:")
+predict_image(model.model, 'test_data/fake/sample.jpg')
